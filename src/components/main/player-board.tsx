9cffb6772c491cc19d23d3af75325081eec00c3a
Sharkri <sharkdude955@gmail.com>
2025-01-21 15:31:59 -0600

    Add log state button

--- src/components/main/player-board.tsx
+++ src/components/main/player-board.tsx
@@ -1,6 +1,7 @@
 import { Round } from "@/lib/types/ttrm";
 import BoardCanvas from "./board-canvas";
 import { GameState } from "@/lib/types/game-state";
+import { Button } from "../ui/button";
 
 export const PlayerBoard = ({
   player,
@@ -12,16 +13,13 @@ export const PlayerBoard = ({
   return (
     <div>
       {player.username}
-      {/* <div className="overflow-scroll max-w-14 whitespace-nowrap">
-        {JSON.stringify(
-          {
-            garbage: gameState.garbageQueued,
-          },
-          null,
-          2
-        )}
-      </div> */}
-      b2b: {gameState.b2b}
+      <Button
+        onClick={() => {
+          console.log(gameState);
+        }}
+      >
+        Log state
+      </Button>
       <BoardCanvas gameState={gameState} />
     </div>
   );
